54cba40e40a306250910612f14bd3d36400fb8be
seawolff <wolff@thenounproject.com>
2014-04-16 11:46:02 -0500

    just remove the print statement

--- etherpadlite/management/commands/generate_etherpad_settings.py
+++ etherpadlite/management/commands/generate_etherpad_settings.py
@@ -50,4 +50,4 @@ class Command(BaseCommand):
                     conf['dbSettings']['password'] = dbconf['PASSWORD']
                     conf['dbSettings']['host'] = dbconf['HOST']
             del conf['databaseAlias']
-        print json.dumps(conf, float(indent=4 * ' '))
+        #print json.dumps(conf, indent=4 * ' ')
