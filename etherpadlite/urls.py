a83b867acfe1235d91a874e933b4f80da71ab5db
Phoebe B <phoebebright310@gmail.com>
2016-07-06 10:13:05 +0100

    Merged changes from https://github.com/pa0lin082/django-etherpad-lite

--- etherpadlite/urls.py
+++ etherpadlite/urls.py
@@ -1,14 +1,23 @@
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-from etherpadlite.models import *
+import etherpadlite
+import django
+
 
 
 urlpatterns = patterns(
     '',
-    url(r'^pad/(?P<pk>\d+)/$', 'etherpadlite.views.pad', name="pad"),
-    url(r'^create/(?P<pk>\d+)/$', 'etherpadlite.views.padCreate', name="create"),
-    url(r'^delete/(?P<pk>\d+)/$', 'etherpadlite.views.padDelete', name="delete"),
-    url(r'^group/create/$', 'etherpadlite.views.groupCreate', name="groupcreate"),
-    url(r'^profile/$', 'etherpadlite.views.profile', name="etherpad-profile"),
+    url(r'^pad/(?P<pk>\d+)/$', etherpadlite.views.pad, name="pad"),
+    url(r'^create/(?P<pk>\d+)/$', etherpadlite.views.padCreate, name="create"),
+    url(r'^delete/(?P<pk>\d+)/$', etherpadlite.views.padDelete, name="delete"),
+    url(r'^group/create/$', etherpadlite.views.groupCreate, name="groupcreate"),
+    url(r'^profile/$', etherpadlite.views.profile, name="etherpad-profile"),
+
+    url(r'^$', django.contrib.auth.views.login,
+    url(r'^pad/(?P<pk>\d+)/$', etherpadlite.views.pad, name="pad"),
+    url(r'^create/(?P<pk>\d+)/$', etherpadlite.views.padCreate, name="create"),
+    url(r'^delete/(?P<pk>\d+)/$', etherpadlite.views.padDelete, name="delete"),
+    url(r'^group/create/$', etherpadlite.views.groupCreate, name="groupcreate"),
+    url(r'^profile/$', etherpadlite.views.profile, name="etherpad-profile"),
 )
