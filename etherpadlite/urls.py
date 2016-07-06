91121a6e0c6ea8116388f8d5a5fa756609ab7b0e
Phoebe B <phoebebright310@gmail.com>
2016-07-06 10:22:27 +0100

    Merged changes from https://github.com/pa0lin082/django-etherpad-lite

--- etherpadlite/urls.py
+++ etherpadlite/urls.py
@@ -14,10 +14,13 @@ urlpatterns = patterns(
     url(r'^group/create/$', etherpadlite.views.groupCreate, name="groupcreate"),
     url(r'^profile/$', etherpadlite.views.profile, name="etherpad-profile"),
 
+
     url(r'^$', django.contrib.auth.views.login,
-    url(r'^pad/(?P<pk>\d+)/$', etherpadlite.views.pad, name="pad"),
-    url(r'^create/(?P<pk>\d+)/$', etherpadlite.views.padCreate, name="create"),
-    url(r'^delete/(?P<pk>\d+)/$', etherpadlite.views.padDelete, name="delete"),
-    url(r'^group/create/$', etherpadlite.views.groupCreate, name="groupcreate"),
-    url(r'^profile/$', etherpadlite.views.profile, name="etherpad-profile"),
+        {'template_name': 'etherpad-lite/login.html'}),
+    url(r'^etherpad$', django.contrib.auth.views.login,
+        {'template_name': 'etherpad-lite/login.html'}),
+    url(r'^logout$', django.contrib.auth.views.logout,
+        {'template_name': 'etherpad-lite/logout.html'}),
+    url(r'^accounts/profile/$', etherpadlite.views.profile),
+
 )
